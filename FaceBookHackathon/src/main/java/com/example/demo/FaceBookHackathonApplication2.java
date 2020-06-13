cb7a6c4e2c214feb7c088f62959cd0aa3b2d6173
RRAMAC22 <RRAMAC22@WGC1004V5K2Z2.chennaigtbc.ford.com>
2020-06-14 01:03:23 +0530

    test

---
+++ FaceBookHackathon/src/main/java/com/example/demo/FaceBookHackathonApplication2.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceBookHackathonApplication2 {
+
+	//Add controllers here
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FaceBookHackathonApplication2.class, args);
+	}
+
+}
