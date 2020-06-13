23174ec26142d60c839730640cdc74d0429b01ea
RRAMAC22 <RRAMAC22@WGC1004V5K2Z2.chennaigtbc.ford.com>
2020-06-14 02:03:42 +0530

    commit

---
+++ FaceBookHackathon/src/main/java/com/controllers/FaceBookController.java
@@ -0,0 +1,15 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FaceBookController {
+	
+	
+//	@GetMapping("/initLoad")
+//	public String LoadPage()
+//	{
+//		
+//	}
+
+}
