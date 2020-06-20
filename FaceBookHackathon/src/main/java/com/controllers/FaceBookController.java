42025394a251739970cdcb4f3b7d9cd2416252eb
RRAMAC22 <RRAMAC22@WGC1004V5K2Z2.chennaigtbc.ford.com>
2020-06-20 18:38:33 +0530

    challenge seven

--- FaceBookHackathon/src/main/java/com/controllers/FaceBookController.java
+++ FaceBookHackathon/src/main/java/com/controllers/FaceBookController.java
@@ -30,9 +30,14 @@ public class FaceBookController {
 	MongoTemplate mongoTemplate;
 	
 	@PostMapping("/upload")
-	public String singleFileUpload(@RequestParam("file1") MultipartFile fileOne,@RequestParam("file2") MultipartFile fileTwo,@RequestParam("file3") MultipartFile fileThree,@RequestParam("file4") MultipartFile fileFour) {
+	public String singleFileUpload(@RequestParam("file1") MultipartFile fileOne,@RequestParam("file2") MultipartFile fileTwo,@RequestParam("file3") MultipartFile fileThree,@RequestParam("file4") MultipartFile fileFour,@RequestParam("file5") MultipartFile fileFive,
+			@RequestParam("file6") MultipartFile fileSix, @RequestParam("file7") MultipartFile fileSeven
+			) 
+	{
+		
+		long count = 0;
 	    try {
-	    	 long count = mongoTemplate.getCollection("hackathonDocument").countDocuments();
+	    	  count = mongoTemplate.getCollection("hackathonDocument").countDocuments();
 	    	 count++;
 	    	HackathonDocument demoDocument = new HackathonDocument();
 	        demoDocument.setId(Long.toString(count));
@@ -61,19 +66,38 @@ public class FaceBookController {
 	        challengeFour.setFile(new Binary(BsonBinarySubType.BINARY, fileFour.getBytes()));
 	        
 	         
+	        
+	        ChallengeDocument challengeFive = new ChallengeDocument();
+	        challengeFive.setChallengeId("5");
+	        challengeFive.setFile(new Binary(BsonBinarySubType.BINARY, fileFive.getBytes()));
+	        
+	        
+	        ChallengeDocument challengeSix = new ChallengeDocument();
+	        challengeSix.setChallengeId("6");
+	        challengeSix.setFile(new Binary(BsonBinarySubType.BINARY, fileSix.getBytes()));
+	        
+	        
+	        ChallengeDocument challengeSeven = new ChallengeDocument();
+	        challengeSeven.setChallengeId("7");
+	        challengeSeven.setFile(new Binary(BsonBinarySubType.BINARY, fileSeven.getBytes()));
+	        
+	        
 	        List<ChallengeDocument> listOfDocuments = new ArrayList<ChallengeDocument>();
 	        listOfDocuments.add(challengeOne);
 	        listOfDocuments.add(challengeTwo);
 	        listOfDocuments.add(challengeThree);
 	        listOfDocuments.add(challengeFour);
+	        listOfDocuments.add(challengeFive);
+	        listOfDocuments.add(challengeSix);
+	        listOfDocuments.add(challengeSeven);
 	        demoDocument.setDocuments(listOfDocuments);
 	        mongoTemplate.insert(demoDocument);
-	        System.out.println(demoDocument);
+	      
 	    } catch (Exception e) {
 	        e.printStackTrace();
 	        return "failure";
 	    }
-	    return "success";
+	    return Long.toString(count);
 	}
 
 }
